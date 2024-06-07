678e2100de0fb967ae94c4a619de24f7d65ace86
romaric <coulromaric@gmail.comexit>
2024-06-07 00:27:42 +0000

    api create issue route okay

---
+++ app/api/issue/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+import {z} from "zod";
+import prisma from "@/prisma/client";
+
+const CreateIssueSchema = z.object({
+    title: z.string().min(1).max(255),
+    description: z.string().min(1),
+});
+
+
+export async function POST (request:NextResponse){
+    const body = await request.json()
+    const validation = CreateIssueSchema.safeParse(body)
+    if (!validation.success) {
+        return NextResponse.json(validation.error.errors,{status:400})
+    }
+    const newIssue = await prisma.issue.create({
+        data:{title:body.title,description:body.description}
+    })
+    return NextResponse.json(newIssue,{status:201})
+    
+}
